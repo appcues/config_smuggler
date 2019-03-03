520ceda024d24384d489c07fedf17025e8a48f6e
pete gamache <pete@gamache.org>
2019-03-03 13:22:52 -0500

    fix for ex_aws config issue (false kwlist)

--- lib/config_smuggler/encoder.ex
+++ lib/config_smuggler/encoder.ex
@@ -6,18 +6,26 @@ defmodule ConfigSmuggler.Encoder do
   """
   @spec encode_app_path_and_opts(atom, [atom], Keyword.t()) ::
           [{ConfigSmuggler.encoded_key(), ConfigSmuggler.encoded_value()}]
-  def encode_app_path_and_opts(app, path, opts) when is_list(opts) do
-    Enum.flat_map(opts, fn {key, value} ->
-      case value do
-        [{_k, _v} | _] -> encode_app_path_and_opts(app, path ++ [key], value)
-        _ -> [{encode_key([app] ++ path ++ [key]), encode_value(value)}]
-      end
-    end)
+  def encode_app_path_and_opts(app, path, opts) do
+    if is_keyword_list?(opts) do
+      Enum.flat_map(opts, fn {key, value} ->
+        case value do
+          [{_k, _v} | _] -> encode_app_path_and_opts(app, path ++ [key], value)
+          _ -> [{encode_key([app] ++ path ++ [key]), encode_value(value)}]
+        end
+      end)
+    else
+      [{encode_key([app | path]), encode_value(opts)}]
+    end
   end
 
-  def encode_app_path_and_opts(app, path, value) do
-    [{encode_key([app | path]), encode_value(value)}]
+  defp is_keyword_list?([]), do: true
+
+  defp is_keyword_list?([{k, _v} | rest]) when is_atom(k) do
+    is_keyword_list?(rest)
   end
+
+  defp is_keyword_list?(_), do: false
 
   @doc ~S"""
   Returns an encoded key using the given parts (atoms and
