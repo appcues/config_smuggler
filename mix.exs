8723c2b19d18a90595b6de31fb0a1a062857b6f5
Andy LeClair <andyleclair@gmail.com>
2021-06-21 13:37:48 -0400

    0.7.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConfigSmuggler.MixProject do
   def project do
     [
       app: :config_smuggler,
-      version: "0.7.2",
+      version: "0.7.3",
       description:
         "ConfigSmuggler converts Elixir-style configuration statements to and from string-encoded key/value pairs.",
       elixir: "~> 1.7",
