6c66b288949ced96a528b25f753ceee8e4025c10
Rubem Nakamura <rubem.nakamura@gmail.com>
2023-09-01 14:33:02 -0300

    bump version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConfigSmuggler.MixProject do
   def project do
     [
       app: :config_smuggler,
-      version: "1.0.0",
+      version: "1.0.1",
       description:
         "ConfigSmuggler converts Elixir-style configuration statements to and from string-encoded key/value pairs.",
       elixir: "~> 1.7",
