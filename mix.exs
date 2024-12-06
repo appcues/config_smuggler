c4bfd9b5686d6030926786ce5a83c480986d65f7
Nico Mihalich <nico@appcues.com>
2024-12-06 14:54:07 -0500

    v1.0.5

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConfigSmuggler.MixProject do
   def project do
     [
       app: :config_smuggler,
-      version: "1.0.4",
+      version: "1.0.5",
       description:
         "ConfigSmuggler converts Elixir-style configuration statements to and from string-encoded key/value pairs.",
       elixir: "~> 1.7",
