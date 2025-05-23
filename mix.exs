e8a226c1c866489d80fb46dc7645c9ce3e73a220
Rubem Nakamura <rubem.nakamura@gmail.com>
2025-05-23 15:18:55 -0300

    version 1.1.1 and deps update

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConfigSmuggler.MixProject do
   def project do
     [
       app: :config_smuggler,
-      version: "1.1.0",
+      version: "1.1.1",
       description:
         "ConfigSmuggler converts Elixir-style configuration statements to and from string-encoded key/value pairs.",
       elixir: "~> 1.18",
